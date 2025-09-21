495926c8304d3ead496a4b66ef42789568bc20e4
Ian Arguello <ianarguello@Ians-MacBook-Air-2.local>
2025-09-21 14:03:46 -0500

    Some message

---
+++ lab3/Program.cs
@@ -0,0 +1,33 @@
+﻿//main
+string day = GetDayOfWeek();
+while (day ! = "stop")
+{
+    string misEvent = EventDecision(day);
+    DisplayEventMessage(day, misEvent);
+    day = GetDayOfWeek;
+}
+//end main
+static string GetDayOfWeek()
+{
+    Console.Writeline("Enter the day of the week. (Monday-Sunday, Enter stop to stop)");
+    return Console.Readline();
+}
+static string EventDecision(string day)
+{
+    if (day = "monday")
+        return "Class";
+        else if(day = "wednesday")
+        return "Class";
+        else if(day = "tuesday")
+        return "AIMS Meeting";
+        else if(day = "friday")
+        return "Lab";
+        else
+        return "No Activities";
+}
+static void DisplayEventMessage (string day, string misEvent);
+if(misEvent = "")
+
+    Console.Writeline("No Activties");
+
+   else Console.Writeline({day}, {misEvent})
